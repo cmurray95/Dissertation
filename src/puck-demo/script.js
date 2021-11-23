3857593d680c5f71d301ecbf24571134d7e4a117
Craig Murray <craig.murray095@gmail.com>
2021-11-23 20:16:17 +0000

    Added tests for accelerometer and device reset

--- src/puck-demo/script.js
+++ src/puck-demo/script.js
@@ -8,11 +8,6 @@ function ledOff() {
     UART.write(writeVal);
 };
 
-
-function connect() {
-  
-}
-
 temp = 0;
 battery = 0;
 function printStatus(){
@@ -25,3 +20,21 @@ function printStatus(){
     });
     document.getElementById("status").innerHTML = "<p> Temperature = " + temp + "</p><p>Battery = " + battery + "%</p>";
 }
+
+function accelGet(){
+    writeVal = `
+    Puck.on('accel', a=>{
+        LED2.write(a.acc.z < 0);
+    });
+    Puck.accelOn();\n
+    `  
+    return writeVal;
+}
+
+function accel(){
+    UART.write(accelGet());
+}
+
+function reset() {
+    UART.write("reset();\n");
+};
