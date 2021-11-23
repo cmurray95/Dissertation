a3cb5f2fa4befe24164dc29a7b706c85666412f9
Craig Murray <craig.murray095@gmail.com>
2021-11-23 19:16:59 +0000

    refactored espruino-test.js

--- src/remote-uploader/espruino-test.js
+++ src/remote-uploader/espruino-test.js
@@ -1,11 +1,8 @@
 function accel(){
-    writeVal = `
     Puck.on('accel', a=>{
         LED2.write(a.acc.z < 0);
     });
-    Puck.accelOn();\n
-    `  
-    UART.write(writeVal);
+    Puck.accelOn();
 }
 
 accel();
